2c3dad10b8dab9a49f2ef1af9f8c619e588ffafe
popoff <pocariflavio@gmail.com>
2020-10-31 15:42:17 +0100

    Rimosse GET api.

--- lib/services/rest_api/book_api.dart
+++ lib/services/rest_api/book_api.dart
@@ -6,40 +6,10 @@ import 'dart:convert';
 
 class BookApi extends Utils {
 
-  int idUser;
-
 
   Future<List<Book>> getBooks() async {
     var response = await http.get(
         '$urlServer/libri/getAll',
-        headers: header
-    );
-
-    return List
-        .from(jsonDecode(response.body)['data'][0])
-        .map((e) => Book.fromJson(e))
-        .toList();
-  }
-
-
-  Future<List<Book>> getUserReservationBooks(Future<int> userId) async {
-    idUser = await userId;
-    var response = await http.get(
-        '$urlServer/prenotazioni/libriUtente/$idUser',
-        headers: header
-    );
-
-    return List
-        .from(jsonDecode(response.body)['data'][0])
-        .map((e) => Book.fromJson(e))
-        .toList();
-  }
-
-
-  Future<List<Book>> getUserToBeReturnedBooks(Future<int> userId) async {
-    idUser = await userId;
-    var response = await http.get(
-        '$urlServer/prenotazioni/daRestituire/$idUser',
         headers: header
     );
 
