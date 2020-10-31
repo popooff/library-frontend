32dd3bd5526bce429a5089f256422c65588a9504
popoff <pocariflavio@gmail.com>
2020-10-31 18:59:09 +0100

    Rimossa GET che ritorna libro dato l'id.

--- lib/services/rest_api/book_api.dart
+++ lib/services/rest_api/book_api.dart
@@ -19,14 +19,4 @@ class BookApi extends Utils {
         .toList();
   }
 
-
-  Future<Book> getBookById(int id) async {
-    var response = await http.get(
-        '$urlServer/libri/getBook/$id',
-        headers: header
-    );
-
-    return Book.fromJson(jsonDecode(response.body)['data'][0][0]);
-  }
-
 }
