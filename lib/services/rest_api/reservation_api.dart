3c0dfedc3bc8d6274258ae153c6c14f069bad001
popoff <pocariflavio@gmail.com>
2020-10-31 17:05:09 +0100

    Aggiunto un paio di bug.

--- lib/services/rest_api/reservation_api.dart
+++ lib/services/rest_api/reservation_api.dart
@@ -1,12 +1,16 @@
 import 'package:library_frontend/models/reservation.dart';
 import 'package:library_frontend/services/utils.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'book_api.dart';
 
 
 class ReservationApi extends Utils {
 
   int idUser;
   Reservation reservation;
+  List<Reservation> list = [];
+  BookApi bookApi;
 
 
   Future<bool> addReservation(Future<int> userId, int idBook) async {
@@ -41,5 +45,45 @@ class ReservationApi extends Utils {
 
     return (response.statusCode == 200) ? true : false;
   }
+
+
+  Future<List<Reservation>> getUserReservation(Future<int> userId) async {
+    idUser = await userId;
+    var response = await http.get(
+        '$urlServer/prenotazioni/libriUtente/$idUser',
+        headers: header
+    );
+
+    return null;
+  }
+
+
+  Future<List<Reservation>> getUserToBeReturned(Future<int> userId) async {
+    idUser = await userId;
+    var response = await http.get(
+        '$urlServer/prenotazioni/daRestituire/$idUser',
+        headers: header
+    );
+
+    var responseList = jsonDecode(response.body)['data'][0];
+
+    for (var el in responseList) {
+      list.add(
+        Reservation(
+          idBook: el['Libro'],
+          idReservation: el['ID'],
+          dateReservation: DateTime.parse(el['DataPrenotazione'].toString().substring(0, 10))
+        )
+      );
+    }
+
+    for (var el in list) {
+      print(el.idBook);
+    }
+
+    return null;
+  }
+// {"Libro":36,"DataPrenotazione":"2020-10-26T00:00:00.000Z","ID":24}
+//   Id libro                                                 Id prenotazione
 
 }
