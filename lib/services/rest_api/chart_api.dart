e78ccf117d124b3f0e9c6acc5cdcbe91b6c69b17
popoff <pocariflavio@gmail.com>
2020-11-02 21:26:45 +0100

    Aggiunte api per la creazione del grafico.

---
+++ lib/services/rest_api/chart_api.dart
@@ -0,0 +1,79 @@
+import 'package:library_frontend/models/chart.dart';
+import 'package:library_frontend/services/utils.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+class ChartApi extends Utils {
+
+  int userId;
+
+
+  Future<List<Chart>> getAllKindUserRead(Future<int> id) async {
+    userId = await id;
+    var response = await http.get(
+        '$urlServer/grafici/getGeneriUtente/$userId',
+        headers: header
+    );
+
+    return List.of(jsonDecode(response.body)['data'][0])
+        .map((e) => Chart.kindChart(e))
+        .toList();
+  }
+
+
+  Future<List<Chart>> getAllMonthUserRead() async {
+    userId = await getUserId();
+    var response = await http.get(
+        '$urlServer/grafici/getNumberLibriMese/$userId',
+        headers: header
+    );
+
+    return List.of(jsonDecode(response.body)['data'][0])
+        .map((e) => Chart.monthChart(e['NumeroLibri'], getMonth(e['Mese'])))
+        .toList();
+  }
+
+
+  int getMonth(String month) {
+
+    switch(month) {
+      case 'January':
+        return DateTime.january;
+
+      case 'February':
+        return DateTime.february;
+
+      case 'March':
+        return DateTime.march;
+
+      case 'April':
+        return DateTime.april;
+
+      case 'May':
+        return DateTime.may;
+
+      case 'June':
+        return DateTime.june;
+
+      case 'July':
+        return DateTime.july;
+
+      case 'August':
+        return DateTime.august;
+
+      case 'September':
+        return DateTime.september;
+
+      case 'October':
+        return DateTime.october;
+
+      case 'November':
+        return DateTime.november;
+
+      default:
+        return DateTime.december;
+    }
+  }
+
+}
