0ea12921f8eac62848119a8415d6483c0e847646
Matteo <matteoa1999@gmail.com>
2021-02-01 16:37:22 +0100

    Rivisto colore login

--- lib/views/authentication/login_page.dart
+++ lib/views/authentication/login_page.dart
@@ -68,7 +68,7 @@ class _LoginPageState extends State<LoginPage> {
             styles: BlobStyles(
               gradient: LinearGradient(
                   colors: [
-                    Colors.red.withOpacity(0.45),
+                    Colors.blueAccent.withOpacity(0.45),
                     Colors.blueAccent.withOpacity(0.25),
                   ]).createShader(Rect.fromLTRB(0, 100, 200, 300)),
             ),
