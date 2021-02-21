fb13b17980c76a0e30cb7c181a8d372535591e0f
popoff <pocariflavio@gmail.com>
2021-02-21 23:56:44 +0100

    Refactoring.

--- lib/views/client_page/info_page/returned_page.dart
+++ lib/views/client_page/info_page/returned_page.dart
@@ -49,53 +49,53 @@ class _ReturnedPageState extends State<ReturnedPage> {
                       side: BorderSide(color: Colors.black38)
                   ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      ListTile(
-                        title: Text('${data[index].book.title.toString()}'),
-                        subtitle: Text('${data[index].book.kind.toString()}'),
-                        leading: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: CachedNetworkImageProvider(
-                            '${reservationApi.urlServer}/download/${data[index].book.cover}',
-                            headers:reservationApi.authHeader(token),
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        ListTile(
+                            title: Text('${data[index].book.title.toString()}'),
+                            subtitle: Text('${data[index].book.kind.toString()}'),
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: CachedNetworkImageProvider(
+                                '${reservationApi.urlServer}/download/${data[index].book.cover}',
+                                headers:reservationApi.authHeader(token),
+                              ),
+                            )
+                        ),
+
+                        Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: FlatButton(
+
+                            onPressed: () async {
+                              bool returned = await reservationApi.returnBook(data[index].idReservation);
+
+                              if (returned) {
+                                setState(() {
+                                  data.removeAt(index);
+                                });
+
+                              } else {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return LibraryAlert(
+                                      type: AlertDialogType.WARNING,
+                                      content: "Problemi con la restituzione del libro!",
+                                    );
+                                  },
+                                );
+                              }
+                            },
+
+                            child: Text('Restituisci', style: TextStyle(color: Colors.black54),),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(color: Colors.black.withOpacity(0.5))
+                            ),
                           ),
                         )
-                      ),
-
-                      Padding(
-                        padding: EdgeInsets.only(right: 5),
-                        child: FlatButton(
-
-                          onPressed: () async {
-                            bool returned = await reservationApi.returnBook(data[index].idReservation);
-
-                            if (returned) {
-                              setState(() {
-                                data.removeAt(index);
-                              });
-
-                            } else {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return LibraryAlert(
-                                    type: AlertDialogType.WARNING,
-                                    content: "Problemi con la restituzione del libro!",
-                                  );
-                                },
-                              );
-                            }
-                          },
-
-                          child: Text('Restituisci', style: TextStyle(color: Colors.black54),),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              side: BorderSide(color: Colors.black.withOpacity(0.5))
-                          ),
-                        ),
-                      )
-                    ]),
+                      ]),
                 );
               })
       ),
